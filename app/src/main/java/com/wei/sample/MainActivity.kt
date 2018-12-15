85f42836268895dfb0b96b05055daf4059ea321c
魏树鑫 <weishuxin@icourt.cc>
2018-12-15 20:38:26 +0800

    修改主界面

--- app/src/main/java/com/wei/sample/MainActivity.kt
+++ app/src/main/java/com/wei/sample/MainActivity.kt
@@ -1,69 +1,22 @@
 package com.wei.sample
 
-import android.net.Uri
-import android.support.v7.app.AppCompatActivity
+import android.app.ListActivity
 import android.os.Bundle
-import java.util.HashMap
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import java.util.*
 
 
-
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : ListActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        main()
+        val itemList = Arrays.asList("测试", "测试2")
+        listAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, itemList)
     }
 
+    override fun onListItemClick(l: ListView?, v: View?, position: Int, id: Long) {
 
-    fun main() {
-        println("地址1:"+buildTaskDetailUrl("1"))
-        val query = HashMap<String, String>()
-        query["id"] = "1"
-        query["id"] = "哈哈"
-        println("地址2:"+buildUrl("/third/task.html", query))
-    }
-
-    /**
-     * 构建任务详情路由地址
-     *
-     * @param taskId 任务id
-     * @return 地址
-     */
-    fun buildTaskDetailUrl(taskId: String): String {
-        try {
-            val builder = Uri.Builder()
-            val parse = Uri.parse("https://alphalawyer.cn/")
-            builder.scheme(parse.scheme)
-            builder.encodedAuthority(parse.authority)
-            builder.path("/third/task.html")
-            builder.encodedQuery("id2=哈哈")
-            return builder.build().toString()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return ""
-    }
-
-    fun buildUrl(path: String, query: HashMap<String, String>?): String {
-        try {
-            val builder = Uri.Builder()
-            val parse = Uri.parse("https://alphalawyer.cn/")
-            builder.scheme(parse.scheme)
-            builder.encodedAuthority(parse.authority)
-            builder.encodedPath(path)
-            if (query != null) {
-                for ((key, value) in query) {
-                    builder.appendQueryParameter(key, value)
-                }
-            }
-            return builder.build().toString()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return ""
     }
 }
